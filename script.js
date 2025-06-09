82afab42d86959b0754d8f5c8a8bbe5d2ffd2520
David Ruiz <30082030+2d11ruiz@users.noreply.github.com>
2025-06-09 18:44:17 -0400

    Initialize files for Rock Paper Scissors project
    Created index.html HTML file.
    Created script.js JavaScript file.
    Added boilerplate and a title to index.html.
    Added link in index.html to script.js.
    Added greeting for Rock Paper Scissors game in console.
    Created function getComputerChoice and added algorithm comment.

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Rock Paper Scissors");
+
+/*
+Computer choice algorithm
+Declare "value" variable with the number calculated below:
+    Get random number between 0 and 1
+    Multiply result by a factor of 3
+Declare "choice" variable
+Determine "choice" value according to the logic below:
+    If "value" is 0 up to but not including 1: assign "rock"
+    If "value" is 1 up to but not including 2: assign "paper"
+    If "value" is 2 up to but not including 3: assign "scissors"
+Return "value" as a representation of the computer's choice
+*/
+function getComputerChoice(){
+    
+}
