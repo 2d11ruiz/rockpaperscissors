10baac04dd09618d473eaf155e5da9fcbeea4f19
David Ruiz <30082030+2d11ruiz@users.noreply.github.com>
2025-06-09 22:34:10 -0400

    Add getHumanChoice function code
    Added getHumanChoice function algorithm comment.
    Added getHumanChoice function code.

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
 console.log("Rock Paper Scissors");
 
+console.log(`Computer chose: ${getComputerChoice()}`);
+
+console.log(`Human chose: ${getHumanChoice()}`);
+
 /*
 Computer choice algorithm
-Declare "value" variable with the number calculated below:
+Initialize "value" variable with the number calculated below:
     Get random number between 0 and 1
     Multiply result by a factor of 3
-Declare "choice" variable with a string using the logic below:
+Declare "choice" variable 
+Assign "choice" variable with a string using the logic below:
     If "value" is 0 up to but not including 1: assign "rock"
     If "value" is 1 up to but not including 2: assign "paper"
     If "value" is 2 up to but not including 3: assign "scissors"
-Return "choice" as a representation of the computer's choice
+Return "choice"
 */
 function getComputerChoice(){
     let value = Math.random() * 3;
-    console.log(value);
+    let choice;
+
     if (value >= 0 && value < 1){
-        var choice = "rock";
+        choice = "rock";
     } else if (value >= 1 && value < 2){
-        var choice = "paper";
+        choice = "paper";
     } else if (value >= 2 && value < 3){
-        var choice = "scissors";
+        choice = "scissors";
     }
 
     return choice;
 }
 
-console.log(getComputerChoice());
+/*
+Human choice algorithm
+Declare "choice" variable and store user input inside 
+If "choice" variable matches "rock" return "rock"
+If "choice" variable matches "paper" return "paper"
+If "choice" variable matches "scissors" return "scissors"
+*/
+function getHumanChoice(){
+    let choice = prompt("Enter rock, paper, or scissors:");
+    
+    if (choice === "rock"){
+        return "rock";
+    } else if (choice === "paper"){
+        return "paper";
+    } else if (choice === "scissors"){
+        return "scissors";
+    }
+}
