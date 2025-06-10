e64386c888d1d32b91f38f2267fd16d9c3b5aeea
David Ruiz <30082030+2d11ruiz@users.noreply.github.com>
2025-06-09 20:47:39 -0400

    Update algorithm comment and fix syntax errors
    Updated getComputerChoice function algorithm comment.
    Fixed getComputerChoice function syntax errors.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ Computer choice algorithm
 Declare "value" variable with the number calculated below:
     Get random number between 0 and 1
     Multiply result by a factor of 3
-Declare "choice" value according to the logic below:
+Declare "choice" variable with a string using the logic below:
     If "value" is 0 up to but not including 1: assign "rock"
     If "value" is 1 up to but not including 2: assign "paper"
     If "value" is 2 up to but not including 3: assign "scissors"
@@ -15,11 +15,11 @@ function getComputerChoice(){
     let value = Math.random() * 3;
     console.log(value);
     if (value >= 0 && value < 1){
-        choice = "rock";
+        var choice = "rock";
     } else if (value >= 1 && value < 2){
-        choice = "paper";
+        var choice = "paper";
     } else if (value >= 2 && value < 3){
-        choice = "scissors";
+        var choice = "scissors";
     }
 
     return choice;
