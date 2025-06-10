52a1b204bc25ce689ebc2d0ebddda666dc6610d1
David Ruiz <30082030+2d11ruiz@users.noreply.github.com>
2025-06-10 00:36:35 -0400

    Add playRound function
    Added playRound function algorithm comment.
    Added playRound function code.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 console.log("Rock Paper Scissors");
 
 console.log(`Computer chose: ${getComputerChoice()}`);
@@ -40,12 +43,62 @@ If "choice" variable matches "scissors" return "scissors"
 */
 function getHumanChoice(){
     let choice = prompt("Enter rock, paper, or scissors:");
-    
+    choice = choice.toLowerCase();
+
     if (choice === "rock"){
         return "rock";
     } else if (choice === "paper"){
         return "paper";
     } else if (choice === "scissors"){
         return "scissors";
+    }
+}
+
+/*
+Play round algorithm
+If user chooses rock:
+    If computer chooses rock, result is a tie
+    If computer chooses paper, display computer win and increment computer score by one
+    If computer chooses scissors, display human win and increment human score by one
+If user chooses paper:
+    If computer chooses rock, display human win and increment human score by one
+    If computer chooses paper, result is a tie
+    If computer chooses scissors, display computer win and increment computer score by one
+If user chooses scissors:
+    If computer chooses rock, display computer win and increment computer score by one
+    If computer chooses paper, display human win and increment human score by one
+    If computer chooses scissors, result is a tie
+*/
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === "rock"){
+        if (computerChoice === "rock"){
+            console.log("Human: rock, Computer: rock, Result: Tie");
+        } else if (computerChoice === "paper"){
+            console.log("Human: rock, Computer: paper, Result: Computer win");
+            computerScore += 1;
+        } else if (computerChoice === "scissors"){
+            console.log("Human: rock, Computer: scissors, Result: Human win");
+            humanScore += 1;
+        }
+    } else if (humanChoice === "paper"){
+        if (computerChoice === "rock"){
+            console.log("Human: paper, Computer: rock, Result: Human win");
+            humanScore += 1;
+        } else if (computerChoice === "paper"){
+            console.log("Human: paper, Computer: paper, Result: Tie");
+        } else if (computerChoice === "scissors"){
+            console.log("Human: paper, Computer: scissors, Result: Computer win");
+            computerScore += 1;
+        }
+    } else if (humanChoice === "scissors"){
+        if (computerChoice === "rock"){
+            console.log("Human: scissors, Computer: rock, Result: Computer win");
+            computerScore += 1;
+        } else if (computerChoice === "paper"){
+            console.log("Human: scissors, Computer: paper, Result: Human win");
+            humanScore += 1;
+        } else if (computerChoice === "scissors"){
+            console.log("Human: scissors, Computer: scissors, Result: Tie");
+        }
     }
 }
